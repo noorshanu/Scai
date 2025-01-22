84ed0b938780000b35223da94bbd44a81a1ecbb1
noor alam <51040978+noorshanu@users.noreply.github.com>
2025-01-22 14:35:02 +0530

    nbn

--- src/components/MarketAynal.tsx
+++ src/components/MarketAynal.tsx
@@ -27,36 +27,17 @@ const MarketAnalysis = () => {
           <h4 className="text-sm font-semibold text-white mb-4">
             Sentiment Analysis: BTC
           </h4>
-          <div className="flex justify-between items-center mt-2">
+          <div className="flex justify-between items-center mt-2 relative ">
           <img src="images/rate.png" alt=""  />
+          <img 
+            src="images/pol.png" 
+            alt="" 
+            className="w-auto h-auto max-w-full absolute top-[36%] left-[20%] animate-slide-left" 
+          />
           </div>
         </div>
 
-        {/* Other Analysis Section */}
-        {/* <div className="  p-4   ">
-          <h4 className="text-lg font-semibold text-white mb-2">
-            Other Analysis: BTC
-          </h4>
-          <p className="text-sm text-white py-2">As of January 13, 2025, Bitcoin (BTC) has experienced significant 
-          developments:</p>
-          <ul className="list-disc list-inside text-gray-300">
-            <li className="text-sm text-white py-1">
-             Price Milestone: In December 2024, Bitcoin
-              surpassed the $100,000 mark, reaching an all-time high of
-              $103,332.30 on December 4, 2024.{" "}
-           
-            </li>
-         
-            <li className="text-sm text-white py-1">
-             ETF Approvals: The U.S. Securities and Exchange
-              Commission (SEC) approved multiple spot Bitcoin exchange-traded
-              funds (ETF-s) in January 2024.{" "}
-            
-            </li>
-      
-        
-          </ul>
-        </div> */}
+ 
       </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,16 @@ export default {
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
       },
+      keyframes: {
+        'slide-left': {
+          '0%': { left: '20%' },
+          '50%': { left: '60%' },
+          '100%': { left: '20%' }
+        }
+      },
+      animation: {
+        'slide-left': 'slide-left 5s infinite ease-in-out'
+      }
     },
   },
   plugins: [],
