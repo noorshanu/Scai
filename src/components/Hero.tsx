e43cc20b7725a3b7032074172b4dadacb4db1637
noor alam <51040978+noorshanu@users.noreply.github.com>
2025-01-22 18:12:08 +0530

    sds

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,15 +4,15 @@ import ChatScreen from "./ChatScreen";
 
 function Hero() {
   return (
-    <div className=" px-10 pt-5 pb-2 ">
-      <div className="flex justify-between flex-col sm:flex-row sm:gap-1 gap-4">
-     <div className="hidden sm:block">
-        <ChatScreen/>
-     </div>
-     <div className="sm:hidden">
-       <img src="images/chatmob.png" alt="" className=" mx-auto" />
-     </div>
-        <div className=" h-full">
+    <div className="min-h-screen px-10 pt-5 pb-2">
+      <div className="flex justify-between flex-col sm:flex-row sm:gap-1 gap-4 h-full w-full">
+        <div className="hidden sm:block ">
+          <ChatScreen/>
+        </div>
+        <div className="sm:hidden">
+          <img src="images/chatmob.png" alt="" className="mx-auto" />
+        </div>
+        <div className="h-full ">
           <MarketAnalysis />
         </div>
       </div>
